ab9f75a0f3bd3544f5cd2728d3e85886b01342e1
Gilina McBride <gilinamcbride@gmail.com>
2022-07-21 12:28:13 -0400

    created user, blog, comment models and user routes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-// const routes = require("./controllers");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 // const exphbs = require("express-handlebars");
-// const session = require("express-session");
+const session = require("express-session");
 // const helpers = require("./utils/helpers");
 // const hbs = exphbs.create({ });
 
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,23 +15,23 @@ const PORT = process.env.PORT || 3001;
 // app.engine("handlebars", hbs.engine);
 // app.set("view engine", "handlebars");
 
-// const sess = {
-//   secret: "tech blog secret",
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+const sess = {
+  secret: "tech blog secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
