5cdda3424bb78efd910884c72d211737d7dbed74
Daniel Conlon <danielcconlon@gmail.com>
2022-07-22 21:50:01 -0400

    single blog post displaying

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -44,4 +44,51 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/blog/:id', (req, res) => {
+    Blog.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'city_location',
+            'created_at'
+        ],
+
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbBlogData => {
+        if(!dbBlogData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        // serialize the data
+        const post = dbBlogData.get({ plain: true });
+
+        // passing data to the template
+        res.render('single-blog', { post });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
