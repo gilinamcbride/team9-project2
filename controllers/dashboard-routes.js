f556487df58d18cd17bc34e576944bfc89faa231
Daniel Conlon <danielcconlon@gmail.com>
2022-07-23 21:23:02 -0400

    editing a post works

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -44,7 +44,49 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+// route for editing blog post when on the dashboard page
+router.get('/edit/:id', withAuth, (req, res) => {
+    Blog.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'city_location'
+        ],
 
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'blog_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbBlogData => {
+        if(dbBlogData) {
+            const post = dbBlogData.get({ plain: true });
+
+            res.render('edit-post', {
+                post, loggedIn: true
+            });
+        }
+        else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
 
 
 module.exports = router;
